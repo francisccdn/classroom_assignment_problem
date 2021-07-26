9163be4958ef2a2124e7f50b88f454d47af552ec
francisccdn <francisccdn@gmail.com>
2021-07-26 20:46:20 -0300

    skip heuristic in run all script

--- run_all.py
+++ run_all.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 
 
-time_limit = 0  # In minutes. 0 skips solver, < 0 is no time limit.
+time_limit = -1  # In minutes. 0 skips solver, < 0 is no time limit.
 
 
 class bcolors:
@@ -62,23 +62,49 @@ for file in os.listdir(dirpath):
 
 print("Rodando todas as instâncias...")
 
-instances = [20181]  # [20181, 20182, 20191]
+instances = [20181, 20182, 20191]
 
 for scenario in range(1, 11):
     for instance in instances:
-        for heuristic in range(0, 2):
-            args = []
-            time_limit_str = str(time_limit) if heuristic == 0 else "0"
+        # for heuristic in range(0, 2):
+        heuristic = 0
 
-            # No setup
+        args = []
+        time_limit_str = str(time_limit) if heuristic == 0 else "0"
 
-            if [instance, scenario, 0, 0, heuristic] not in found:
-                print(f"{bcolors.OKBLUE}" + "Rodando -- Instancia: " + str(instance) + " Cenario: " +
-                      str(scenario) + " Heuristica: " + str(heuristic) + " Sem setup" + f"{bcolors.ENDC}")
+        # No setup
 
-                args = ['./cap', str(instance), str(scenario), "0", "0", time_limit_str, str(heuristic)]
-                logname = str(instance) + "_" + str(scenario)
+        if [instance, scenario, 0, 0, heuristic] not in found:
+            print(f"{bcolors.OKBLUE}" + "Rodando -- Instancia: " + str(instance) + " Cenario: " +
+                    str(scenario) + " Heuristica: " + str(heuristic) + " Sem setup" + f"{bcolors.ENDC}")
 
+            args = ['./cap', str(instance), str(scenario), "0", "0", time_limit_str, str(heuristic)]
+            logname = str(instance) + "_" + str(scenario)
+
+            logname = logname + "_heuristic" if heuristic == 1 else logname
+
+            # Run cap
+            result = subprocess.run(args, capture_output=True)
+            # Save log to file
+            with open("results_log/" + logname + ".log", 'w') as f:
+                f.write(result.stdout.decode("utf8"))
+
+        else:  # If results were already found
+            print(f"{bcolors.OKGREEN}" + "Instancia: " + str(instance) + " Cenario: " + str(scenario) +
+                    " Heuristica: " + str(heuristic) + " Sem setup \t\t Resultados gerados encontrados" + f"{bcolors.ENDC}")
+
+        # With setup
+
+        for setup_before in range(0, 2):
+
+            if [instance, scenario, 1, setup_before, heuristic] not in found:
+                print(f"{bcolors.OKBLUE}" + "Rodando -- Instancia: " + str(instance) + " Cenario: " + str(
+                    scenario) + " Heuristica: " + str(heuristic) + " Setup antes: " + str(setup_before) + f"{bcolors.ENDC}")
+
+                args = ['./cap', str(instance), str(scenario), "1", str(setup_before), time_limit_str, str(heuristic)]
+                logname = str(instance) + "_" + str(scenario) + "_setup"
+
+                logname = logname + "_duringclass" if setup_before == 0 else logname
                 logname = logname + "_heuristic" if heuristic == 1 else logname
 
                 # Run cap
@@ -88,32 +114,8 @@ for scenario in range(1, 11):
                     f.write(result.stdout.decode("utf8"))
 
             else:  # If results were already found
-                print(f"{bcolors.OKGREEN}" + "Instancia: " + str(instance) + " Cenario: " + str(scenario) +
-                      " Heuristica: " + str(heuristic) + " Sem setup \t\t Resultados gerados encontrados" + f"{bcolors.ENDC}")
-
-            # With setup
-
-            for setup_before in range(0, 2):
-
-                if [instance, scenario, 1, setup_before, heuristic] not in found:
-                    print(f"{bcolors.OKBLUE}" + "Rodando -- Instancia: " + str(instance) + " Cenario: " + str(
-                        scenario) + " Heuristica: " + str(heuristic) + " Setup antes: " + str(setup_before) + f"{bcolors.ENDC}")
-
-                    args = ['./cap', str(instance), str(scenario), "1", str(setup_before), time_limit_str, str(heuristic)]
-                    logname = str(instance) + "_" + str(scenario) + "_setup"
-
-                    logname = logname + "_duringclass" if setup_before == 0 else logname
-                    logname = logname + "_heuristic" if heuristic == 1 else logname
-
-                    # Run cap
-                    result = subprocess.run(args, capture_output=True)
-                    # Save log to file
-                    with open("results_log/" + logname + ".log", 'w') as f:
-                        f.write(result.stdout.decode("utf8"))
-
-                else:  # If results were already found
-                    print(f"{bcolors.OKGREEN}" + "Instancia: " + str(instance) + " Cenario: " + str(scenario) + " Heuristica: " + str(
-                        heuristic) + " Setup antes: " + str(setup_before) + " \t Resultados gerados encontrados" + f"{bcolors.ENDC}")
+                print(f"{bcolors.OKGREEN}" + "Instancia: " + str(instance) + " Cenario: " + str(scenario) + " Heuristica: " + str(
+                    heuristic) + " Setup antes: " + str(setup_before) + " \t Resultados gerados encontrados" + f"{bcolors.ENDC}")
 
 # Done!
 print(f"{bcolors.OKBLUE}" +
